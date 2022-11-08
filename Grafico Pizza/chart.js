162cb480033aa6384af8cd23fc2ef6c6a4a186f3
Giboy PiresParker <112408991+pescocin69@users.noreply.github.com>
2022-11-08 10:45:05 -0300

    Add files via upload

---
+++ Grafico Pizza/chart.js
@@ -0,0 +1,24 @@
+google.charts.load('current',{packages: ['corechart']});
+google.charts.setOnLoadCallback(desenharGrafico);
+
+function desenharGrafico() {
+
+
+    let dados = google.visualization.arrayToDataTable([
+        ['Mencoes', 'Quantidade'],
+        ['MB',54],
+        ['B',75],
+        ['R',22],
+        ['I',5]
+    ]);
+   
+    let opcoes = {
+    title: 'Menções Finais dos Alunos da Etec em 2022',
+    colors: ['black','red','blue','pink']
+    //legend: 'none'
+    };
+  
+    let chart = new google.visualization.PieChart(document.getElementById('grafico'));
+    chart.draw(dados, opcoes);
+    
+}
